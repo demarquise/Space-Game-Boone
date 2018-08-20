3d8b9869c0923be5d2a325016d6f8bea1774683c
demarquise boone <demarquise_boone@yahoo.com>
2018-08-20 12:57:46 -0400

    var

--- Space_Game/Program.cs
+++ Space_Game/Program.cs
@@ -10,6 +10,13 @@ namespace Space_Game
     {
         static void Main(string[] args)
         {
-        }
+            var player = new Player();
+            var intro = new IntroScreen(player);
+            var nextScreen = intro.Run();
+
+            while (nextScreen != null)
+            {
+                nextScreen = nextScreen.Run();
+            }   }
     }
 }
